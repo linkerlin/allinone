225d2905b2486709234763844a5586804ab14c7b
linker <linker@linuxvm.(none)>
2013-02-05 18:05:33 +0800

    added args support to spiders

--- spiders/mininova/mininova/spiders/nove.py
+++ spiders/mininova/mininova/spiders/nove.py
@@ -8,12 +8,15 @@ from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider
 
 class MininovaSpider(CrawlSpider):
-
     name = 'nova'
     allowed_domains = ['mininova.org']
-    start_urls = ['http://www.mininova.org/']
+    base_url = 'http://www.mininova.org/'
+    start_urls=[]
     rules = [Rule(SgmlLinkExtractor(allow=['/tor/\d+']), 'parse_item')]
 
+    def __init__(self,urlpath=""):
+        self.start_urls.append( self.base_url+urlpath )
+        super(MininovaSpider,self).__init__()
     def parse_item(self, response):
         print "parse item"
         x = HtmlXPathSelector(response)
@@ -22,5 +25,5 @@ class MininovaSpider(CrawlSpider):
         torrent['name'] = x.select("//h1/text()").extract()
         torrent['description'] = x.select("//div[@id='description']").extract()
         torrent['size'] = x.select("//div[@id='info-left']/p[2]/text()[2]").extract()
-        print "return a torrent:",torrent
+        #print "return a torrent:",torrent
         return torrent
